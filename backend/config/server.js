53e4437376c6dc20a269f1cf63abc682ed36fac0
ToanHo <toanbku@gmail.com>
2021-01-10 12:23:53 +0700

    Initial commit (second time)

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31ba531cbe24680ed927dc84f5bb2849'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd895efbb-4497-4cd5-ad85-1c8e619a4d1e'
+};
