8e34790395e3206a8a9c2e29a475029f1f4fedc2
ToanHo <toanbku@gmail.com>
2021-01-10 16:47:32 +0700

    config backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    url: 'dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '31ba531cbe24680ed927dc84f5bb2849'),
     },
